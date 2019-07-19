cc5daacaaa0bf488385e29cb0af2965526d470a3
Kevin Krupp <51412489+Kevinkrupp@users.noreply.github.com>
2019-07-19 09:32:40 -0400

    Update HISmonitor.ino

--- HISmonitor.ino
+++ HISmonitor.ino
@@ -1,7 +1,15 @@
+#include <time.h>
 
-
-//B0 will be used to start the timer
-double hoursCollected = 0;
+//Reported Variable(required double) to GoogleSheetAPI
+double TotalHeatTime = 0;
+//recorded unix time the pin went HIGH
+float HeatStartTime = 0;
+//recorded unix time the pun went LOW
+float HeatEndTime = 0;
+//calculated deltaTIME between Start and End
+float DeltaHeatTime = 0;
+//Blue led on board
+int led = D7;
 //SD is "set" on latching relay, NO contact. When High for 1sec it will shutdown heat.
 int SD = B3;
 //SD is "reset" on latching relay, NC contact. When High for 1sec it will Enable heat.
@@ -9,18 +17,53 @@ int SDreset = B5;
 
 
 void setup() {
- 
-    pinMode(hoursCollected, INPUT);
+   //Declaring B0 as input
+    pinMode(B0, INPUT);
+
+    //Start with on-board led off
+    pinMode(led, OUTPUT);
+    digitalWrite(led, LOW);
+    
+    //No power to lating relay unless done by admin
     pinMode(SD, OUTPUT);
-    pinMode(SDreset, OUTPUT);
     digitalWrite(SD, LOW);
+    
+    ////No power to lating relay unless done by admin
+    pinMode(SDreset, OUTPUT);
     digitalWrite(SDreset, LOW);
     
     Particle.function("toggleSD", toggleSD);
-    
     Particle.function("toggleSDreset", toggleSDreset);
+    Particle.variable("TotalHeatTime", TotalHeatTime);
     
-    }
+/*
+Start Hour Meter Code */
+ 
+  do {
+        digitalWrite(led, HIGH);
+        //HeatStartTime=time_t();
+    }   while(digitalRead(B0));
+       delay(150);
+       digitalWrite(led, LOW);
+}
+    //While B0==true
+//    if (digitalRead(B0)); 
+    
+    //Record time_t and store it in HeatStartTime(float)
+//    time_t("rightNow") = HeatStartTime();
+   
+    //If B0==false, record time_t and store it in HeatEndTime(float)
+//    if B0 == false();
+    
+    //If HeatStartTime < HeatEndTime 
+//    time_t = HeatEndTime();
+      
+    //Calculate the DeltaTime and store it in DeltaHeatTime(float)
+// (HeatStartTime - HeatEndTime) = DeltaHeatTime();
+  
+    //Add DeltaHeatTime to TotalHeatTime
+//    digitalWrite(DeltaHeatTime, TotalHeatTime);
+
 
 //Command to Shutdown with the "set" on latching relay
 int toggleSD(String command){
@@ -45,6 +88,14 @@ int toggleSDreset(String command){
     return 1;
     }
 
+
 void loop() {
-   
+    /*
+    while (digitalRead(B0)){
+    digitalWrite(led, HIGH);
+    }
+    else {
+    digitalWrite(led, LOW);
+    }
+    */
 }
